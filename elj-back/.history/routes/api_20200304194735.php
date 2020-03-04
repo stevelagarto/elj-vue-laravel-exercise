d5ce196ed2fee7a01a5083f7a91e872efeffc5ae
stevelagarto <stevenzgz@gmail.com>
2020-03-04 20:13:11 +0100

    First Commit/Setting Laravel and Vue Enviroment

---
+++ elj-back/.history/routes/api_20200304194735.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    echo "inside";
+    return $request->user();
+});
+
+Route::post('image', function ($request) {
+    print_r($request);
+    return $request;
+});
+
+Route::get('/', function () {
+    return 'Hello World';
+});
+
+
+
---
+++ elj-back/.history/routes/web_20200304180936.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::post('/image/{id}', function ($id) {
+    //echo 'inside';
+    //$entityBody = file_get_contents('php://input');
+    return $id;
+});
+
